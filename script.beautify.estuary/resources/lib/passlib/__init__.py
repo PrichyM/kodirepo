bbba8fd029b7d17479d17d97bcc5f1ad8a5d8ea6
PrichyM <prichym@gmail.com>
2024-10-13 23:07:48 +0200

    import requests fix

--- script.beautify.estuary/resources/lib/passlib/__init__.py
+++ script.beautify.estuary/resources/lib/passlib/__init__.py
@@ -1,7 +1,7 @@
 """passlib - suite of password hashing & generation routines"""
 import sys, os
 
-packagepath = os.getcwd() + 'passlib'
+packagepath = os.getcwd() + '/passlib'
 sys.path.append(packagepath)
 
 __version__ = '1.7.4'
